ef5903cf39f118aae2ab9280ad7aa69c640bed29
Mahesh <maheshenator@gmail.com>
2024-04-27 20:24:45 +0200

    comment update

--- LocalNotificationsTest/MainPage.xaml.cs
+++ LocalNotificationsTest/MainPage.xaml.cs
@@ -25,7 +25,7 @@ namespace LocalNotificationsTest
         private void InitLocalNotification()
         {
             /*
-             * For Android 13 and above.. This plugin doesnt 
+             * For Android 13 and above.. This plugin doesnt schedule
              1.  Below 3 permissions are turned on in this app manifest 
                     POST_NOTIFICATIONS" , SCHEDULE_EXACT_ALARM , USE_EXACT_ALARM 
                 
